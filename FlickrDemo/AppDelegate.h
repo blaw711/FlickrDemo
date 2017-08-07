e49433f64f96480a351ea48be271bb0ca8467d2a
blaw711 <blaw711@comcast.net>
2017-08-07 16:38:24 -0400

    Initial Commit

---
+++ FlickrDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrDemo
+//
+//  Created by Bob Law on 8/7/17.
+//  Copyright © 2017 Bob Law. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrDemo
+//
+//  Created by Bob Law on 8/7/17.
+//  Copyright © 2017 Bob Law. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
